9f1ad680eff220e25ae9590ea66b68a59b853f15
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-11-12 16:15:10 -0800

    Opening a blank red window... openGL is working.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,68 @@
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-
+const GLint WIDTH = 800, HEIGHT = 600;
 
 int main() 
 {
+	//Initialize GLFW
+	if (!glfwInit()) {
+		printf("GLFW Initialization failed!");
+		glfwTerminate();
+		return 1;
+	}
 	
+	//Setup GLFW window properties
+	//OpenGL Version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//Core profile = no backwards compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//Forward Compat allowed!
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	//Create the window itself
+	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+	if (!mainWindow) {
+		printf("GLFW window creation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	//Get the Buffer size information
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	//Set the context for GLEW to use
+	//This window is the one the openGL context should be tied to...
+	glfwMakeContextCurrent(mainWindow);
+
+	//Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		printf("GLEW initialization failed");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	//Setup Viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+
+	//Loop until the window is closed
+	while (!glfwWindowShouldClose(mainWindow)) {
+		//Get or Handle user input event
+		glfwPollEvents();
+
+		//Clear the window
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
+
 	return 0;
 }
